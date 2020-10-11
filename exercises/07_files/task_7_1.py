3df61912632599c6f9433d4a960a052df9881cd4
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2020-10-11 06:06:46 -0500

    7.1 done

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Задание 7.1
@@ -13,3 +14,22 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+with open('ospf.txt', 'r') as text:
+    ospf = text.readlines()
+    for line in ospf:
+        line = line.strip().split()
+        prefix = line[1]
+        ad = line[2].strip('[]')
+        n_hop = line[4].rstrip(',')
+        l_update = line[5].rstrip(',')
+        o_int = line[6]
+        result = '''
+        Prefix                {0:18}
+        AD/Metric             {1:16}
+        Next-Hop              {2:16}
+        Last update           {3:16}
+        Outbound Interface    {4:16}
+        '''
+
+        print(result.format(prefix, ad, n_hop, l_update, o_int))
