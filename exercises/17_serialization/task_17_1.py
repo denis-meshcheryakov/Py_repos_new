3d426051c684bd995e8b372900cbe3924ccfef25
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-05-05 23:34:19 +0300

    task_17_1.py Done

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -28,3 +28,26 @@ sw3,00:E9:22:11:A6:50,100.1.1.7,3,FastEthernet0/21
 Первый столбец в csv файле имя коммутатора надо получить из имени файла, остальные - из содержимого в файлах.
 
 """
+
+import csv
+import re
+import glob
+
+def write_dhcp_snooping_to_csv(filenames, output):
+    regex = r'(\S+) +(\S+) +\d+ +\S+ +(\d+) +(\S+)'
+    with open(output, 'w') as result_file:
+        writer = csv.writer(result_file)
+        writer.writerow(['switch', 'mac', 'ip', 'vlan', 'interface'])
+        for filename in filenames:
+            switch = re.search(r'([^/]+)_dhcp_snooping.txt', filename).group(1)
+            with open(filename) as f:
+                for line in f:
+                    match = re.search(regex, line)
+                    if match:
+                        writer.writerow((switch,) + match.groups())
+
+
+if __name__ == '__main__':
+    sh_dhcp_snoop_files = glob.glob('*_dhcp_snooping.txt')
+    write_dhcp_snooping_to_csv(sh_dhcp_snoop_files, 'result_csv.csv')
+
