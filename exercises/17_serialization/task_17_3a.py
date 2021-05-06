33a3a88ec4bff3dffd3c9bcb5ef10e63b0ad2eab
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-05-06 19:15:49 +0300

    task_17_3a.py Done

--- exercises/17_serialization/task_17_3a.py
+++ exercises/17_serialization/task_17_3a.py
@@ -2,7 +2,8 @@
 """
 Задание 17.3a
 
-Создать функцию generate_topology_from_cdp, которая обрабатывает вывод команды show cdp neighbor из нескольких файлов и записывает итоговую топологию в один словарь.
+Создать функцию generate_topology_from_cdp, которая обрабатывает вывод команды show cdp neighbor из нескольких файлов и
+записывает итоговую топологию в один словарь.
 
 Функция generate_topology_from_cdp должна быть создана с параметрами:
 * list_of_files - список файлов из которых надо считать вывод команды sh cdp neighbor
@@ -10,7 +11,8 @@
  * значение по умолчанию - None. По умолчанию, топология не сохраняется в файл
  * топология сохраняется только, если save_to_filename как аргумент указано имя файла
 
-Функция должна возвращать словарь, который описывает соединения между устройствами, независимо от того сохраняется ли топология в файл.
+Функция должна возвращать словарь, который описывает соединения между устройствами, независимо от того сохраняется ли
+топология в файл.
 
 Структура словаря должна быть такой:
 {'R4': {'Fa 0/1': {'R5': 'Fa 0/1'},
@@ -32,3 +34,23 @@
 Проверить работу параметра save_to_filename и записать итоговый словарь в файл topology.yaml.
 
 """
+import yaml
+import glob
+from pprint import pprint
+from task_17_3 import parse_sh_cdp_neighbors
+
+
+def generate_topology_from_cdp(list_of_files, save_to_filename=None):
+    topology = {}
+    for filename in list_of_files:
+        with open(filename) as f:
+            topology.update(parse_sh_cdp_neighbors(f.read()))
+    if save_to_filename:
+        with open(save_to_filename, 'w') as f_out:
+            yaml.dump(topology, f_out, default_flow_style=False)
+    return topology
+
+
+if __name__ == '__main__':
+    f_list = glob.glob('sh_cdp_n_*')
+    pprint(generate_topology_from_cdp(f_list, save_to_filename='topology.yaml'))
