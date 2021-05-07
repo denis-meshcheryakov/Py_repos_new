3f4f3d971fdac0cd5355c2e2f2971ed92a2f5f7f
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-05-07 18:14:11 +0300

    task_17_4.py Done

--- exercises/17_serialization/task_17_4.py
+++ exercises/17_serialization/task_17_4.py
@@ -38,7 +38,7 @@ C-3PO,c3po@gmail.com,16/12/2019 17:24
 Функции convert_str_to_datetime и convert_datetime_to_str использовать не обязательно.
 
 """
-
+import csv
 import datetime
 
 
@@ -54,3 +54,24 @@ def convert_datetime_to_str(datetime_obj):
     Конвертирует строку с датой в формате 11/10/2019 14:05 в объект datetime.
     """
     return datetime.datetime.strftime(datetime_obj, "%d/%m/%Y %H:%M")
+
+
+def write_last_log_to_csv(source_log, output):
+    with open(source_log) as f:
+        data = list(csv.reader(f))
+        header = data[0]
+    result = {}
+    sorted_by_date = sorted(
+        data[1:], key=lambda x: convert_str_to_datetime(x[2])
+    )
+    for name, email, date in sorted_by_date:
+        result[email] = (name, email, date)
+    with open(output, "w") as dest:
+        writer = csv.writer(dest)
+        writer.writerow(header)
+        for row in result.values():
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    write_last_log_to_csv("mail_log.csv", "example_result.csv")
