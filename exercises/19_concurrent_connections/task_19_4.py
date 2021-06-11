44363e3986a6ced17eb8650d1fdbe59aa217817b
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-06-11 16:28:13 -0500

    task_19_4.py Done

--- exercises/19_concurrent_connections/task_19_4.py
+++ exercises/19_concurrent_connections/task_19_4.py
@@ -2,7 +2,8 @@
 """
 Задание 19.4
 
-Создать функцию send_commands_to_devices, которая отправляет команду show или config на разные устройства в параллельных потоках, а затем записывает вывод команд в файл.
+Создать функцию send_commands_to_devices, которая отправляет команду show или config
+на разные устройства в параллельных потоках, а затем записывает вывод команд в файл.
 
 Параметры функции:
 * devices - список словарей с параметрами подключения к устройствам
@@ -86,3 +87,43 @@ R3#
 
 Для выполнения задания можно создавать любые дополнительные функции.
 """
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from netmiko import ConnectHandler
+import yaml
+
+
+def send_show_command(device, command):
+    with ConnectHandler(**device) as ssh:
+        ssh.enable()
+        result = ssh.send_command(command)
+        prompt = ssh.find_prompt()
+    return f'{prompt}{command}\n{result}\n'
+
+
+def send_cfg_commands(device, commands):
+    with ConnectHandler(**device) as ssh:
+        ssh.enable()
+        result = ssh.send_config_set(commands)
+    return f'{result}\n'
+
+
+def send_commands_to_devices(devices, filename, *, show=None, config=None, limit=3):
+    if show and config:
+        raise ValueError("Можно передавать только один из аргументов show/config")
+    command = show if show else config
+    function = send_show_command if show else send_cfg_commands
+
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        futures = [executor.submit(function, device, command) for device in devices]
+        with open(filename, 'w') as f:
+            for future in as_completed(futures):
+                f.write(future.result())
+
+
+if __name__ == '__main__':
+    command = 'sh ip int brie'
+    with open('devices.yaml') as f:
+        devices = yaml.safe_load(f)
+    send_commands_to_devices(devices, show=command, filename='result_19_4.txt')
+    send_commands_to_devices(devices, config='logging 10.5.5.5', filename='result_19_4_a.txt')
