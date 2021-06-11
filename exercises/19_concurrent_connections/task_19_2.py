aed4d489102ede2a738ef94de70bd1b5edf39345
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-06-11 12:05:35 -0500

    task_19_2.py Done

--- exercises/19_concurrent_connections/task_19_2.py
+++ exercises/19_concurrent_connections/task_19_2.py
@@ -34,3 +34,31 @@ Ethernet0/1                unassigned      YES NVRAM  administratively down down
 
 Проверить работу функции на устройствах из файла devices.yaml
 """
+from itertools import repeat
+from concurrent.futures import ThreadPoolExecutor
+
+from netmiko import ConnectHandler
+import yaml
+
+
+def send_show_command(device, command):
+    with ConnectHandler(**device) as ssh:
+        # ssh.enabel()
+        result = ssh.send_command(command)
+        prompt = ssh.find_prompt()
+    return f'{prompt}{command}\n{result}\n'
+
+
+def send_show_command_to_devices(devices, command, filename, limit=3):
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        results = executor.map(send_show_command, devices, repeat(command))
+        with open(filename, 'w') as f:
+            for output in results:
+                f.write(output)
+
+
+if __name__ == '__main__':
+    command = 'sh ip int brie'
+    with open('devices.yaml') as f:
+        devices = yaml.safe_load(f)
+    send_show_command_to_devices(devices, command, 'result.txt')
