c40c5b1cc9799a18021d21b77488bc6cfd21e14c
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-06-14 10:52:20 -0500

    task_19_4.py Done

--- exercises/20_jinja2/task_20_1.py
+++ exercises/20_jinja2/task_20_1.py
@@ -13,7 +13,18 @@
 Проверить работу функции на шаблоне templates/for.txt и данных из файла data_files/for.yml.
 
 """
+import os
+from jinja2 import Environment, FileSystemLoader
 import yaml
+
+
+def generate_config(template, data_dict):
+    templ_dir, templ_file = os.path.split(template)
+    env = Environment(
+        loader=FileSystemLoader(templ_dir), trim_blocks=True, lstrip_blocks=True
+    )
+    templ = env.get_template(templ_file)
+    return templ.render(data_dict)
 
 
 # так должен выглядеть вызов функции
