9771c92d5d39bb493b93d36351e7acfc28ff36fd
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-06-14 11:01:34 -0500

    task_20_2.py Done

--- exercises/20_jinja2/task_20_1.py
+++ exercises/20_jinja2/task_20_1.py
@@ -29,8 +29,8 @@ def generate_config(template, data_dict):
 
 # так должен выглядеть вызов функции
 if __name__ == "__main__":
-    data_file = "data_files/for.yml"
-    template_file = "templates/for.txt"
+    data_file = "data_files/router_info.yml"
+    template_file = "templates/cisco_router_base.txt"
     with open(data_file) as f:
         data = yaml.safe_load(f)
     print(generate_config(template_file, data))
--- exercises/20_jinja2/task_20_2.py
+++ exercises/20_jinja2/task_20_2.py
@@ -16,3 +16,9 @@
 В качестве данных, используйте информацию из файла data_files/router_info.yml
 
 """
+# templates/cisco_router_base.txt
+"""
+{% include 'cisco_base.txt' %}
+{% include 'alias.txt' %}
+{% include 'eem_int_desc.txt' %}
+"""
