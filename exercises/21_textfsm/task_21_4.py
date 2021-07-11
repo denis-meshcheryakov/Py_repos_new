f31ae5ee75a5a1bdb32e29c5cfa0c517d7a3cb9f
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-07-11 10:42:08 -0500

    task_21_4.py Done

--- exercises/21_textfsm/task_21_4.py
+++ exercises/21_textfsm/task_21_4.py
@@ -6,7 +6,7 @@
 
 Параметры функции:
 * device_dict - словарь с параметрами подключения к одному устройству
-* command - команда, которую надо выполнить
+* com_mand - команда, которую надо выполнить
 * templates_path - путь к каталогу с шаблонами TextFSM
 * index - имя индекс файла, значение по умолчанию "index"
 
@@ -17,7 +17,7 @@
 * ключи - имена переменных в шаблоне TextFSM
 * значения - части вывода, которые соответствуют переменным
 
-Проверить работу функции на примере вывода команды sh ip int br и устройствах из devices.yaml.
+Проверить работу функции на примере вывода команды sh ip int br и устройствах из dev_ices.yaml.
 """
 import os
 from pprint import pprint
@@ -26,20 +26,20 @@ import yaml
 from task_21_3 import parse_command_dynamic
 
 
-# def send_and_parse_show_command(device_dict, command, templates_path):
+# def send_and_parse_show_command(device_dict, com_mand, templates_path):
 #     if "NET_TEXTFSM" not in os.environ:
 #         os.environ["NET_TEXTFSM"] = templates_path
 #     with ConnectHandler(**device_dict) as ssh:
 #         ssh.enable()
-#         output = ssh.send_command(command, use_textfsm=True)
+#         output = ssh.send_command(com_mand, use_textfsm=True)
 #     return output
 #
 #
 # if __name__ == "__main__":
 #     full_pth = os.path.join(os.getcwd(), 'templates')
-#     with open('devices.yaml') as f:
-#         devices = yaml.safe_load(f)
-#     for device in devices:
+#     with open('dev_ices.yaml') as f:
+#         dev_ices = yaml.safe_load(f)
+#     for device in dev_ices:
 #         result = send_and_parse_show_command(
 #             device, 'sh ip int brie', templates_path=full_pth
 #         )
